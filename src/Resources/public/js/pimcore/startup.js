53abf56268a38ffeba2e5c9830faff0610b1d413
Mariusz Gomse <mgomse.updit@gmail.com>
2017-10-22 23:26:18 +0200

    [#1] Dummy "Add to Clipboard" in the tree menu

--- src/Resources/public/js/pimcore/startup.js
+++ src/Resources/public/js/pimcore/startup.js
@@ -9,19 +9,69 @@ pimcore.plugin.DivanteClipboardBundle = Class.create(pimcore.plugin.admin, {
         pimcore.plugin.broker.registerPlugin(this);
     },
 
-    pimcoreReady: function (params, broker) {
-        var extrasMenu = pimcore.globalmanager.get("layout_toolbar").extrasMenu;
+    pimcoreReady: function () {
+        var user = pimcore.globalmanager.get("user");
+        if (user.isAllowed("objects") && this.isEnabledInPerspective()) {
+            var toolbar = pimcore.globalmanager.get("layout_toolbar");
+            var clipboardToolbarItem = {
+                text: t("divante_clipboard"),
+                iconCls: "pimcore_icon_export",
+                handler:  this.showClipboard.bind(this)
+            };
 
-        extrasMenu.insert(1, {
-            text: t("divante_clipboard"),
-            iconCls: "pimcore_icon_export",
-            handler:  this.showClipboard.bind(this)
+            if (toolbar.extrasMenu === undefined) {
+                this.initExtrasMenu(toolbar);
+            }
+
+            var glossaryRecordIndex = toolbar.extrasMenu.items.findIndex("text", t("Glossary"));
+            var clipboardIndex = 0;
+            if (glossaryRecordIndex >= 0) {
+                clipboardIndex = glossaryRecordIndex + 1;
+            }
+            toolbar.extrasMenu.insert(clipboardIndex, clipboardToolbarItem);
+
+        }
+    },
+
+    prepareObjectTreeContextMenu: function(menu, tree, record) {
+        if (record.data.type !== "folder" && record.data.permissions.view && this.isEnabledInPerspective()) {
+            var copyRecordIndex = menu.items.findIndex("text", t("copy"));
+            var clipboardMenuItem = {
+                text: t("divante_clipboard_add"),
+                iconCls: "pimcore_icon_export",
+                handler: this.addObjectToClipboard.bind(this, tree, record)
+            };
+
+            if (copyRecordIndex < 0) {
+                menu.add(clipboardMenuItem);
+            } else {
+                menu.insert(copyRecordIndex, clipboardMenuItem);
+            }
+        }
+    },
+
+    initExtrasMenu: function (toolbar) {
+        var extrasMenu = new Ext.menu.Menu({
+            items: [],
+            shadow: false,
+            cls: "pimcore_navigation_flyout"
         });
+        toolbar.extrasMenu = extrasMenu;
 
+        Ext.get("pimcore_menu_extras").on("mousedown", toolbar.showSubMenu.bind(extrasMenu)).show();
+    },
+
+    isEnabledInPerspective: function() {
+        var perspectiveCfg = pimcore.globalmanager.get("perspective");
+        return perspectiveCfg.inToolbar("extras") && perspectiveCfg.inToolbar("extras.clipboard");
     },
 
     showClipboard: function() {
         pimcore.helpers.showNotification("Clipboard", "Work in-progress", "error", "Quite soon you'll see you here your Clipboard! :)");
+    },
+
+    addObjectToClipboard: function() {
+        pimcore.helpers.showNotification("Clipboard", "Work in-progress", "error", "Quite soon you'll be able to add object to your Clipboard! :)")
     }
 });
 
